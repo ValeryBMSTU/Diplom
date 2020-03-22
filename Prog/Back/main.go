2011cbe1c2147e6d8370ec7517838f93e20c117a
ValeryBMSTU <vitalian42@mail.ru>
2020-03-22 18:36:10 +0300

    module init

--- Prog/Back/main.go
+++ Prog/Back/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/ValeryBMSTU/Diplom/Prog/Back/consts"
+	"github.com/labstack/echo"
 )
 
 func main() {
 	fmt.Println("I love you...")
+
+	e := echo.New()
+
+	e.Logger.Warnf("shutdown " + consts.Addr)
 }
 
